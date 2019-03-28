102f4652439aedfc161aa90d9c3b45b110847781
Patrick <patricki.lombard@gosh.nhs.uk>
2019-03-28 16:36:14 +0000

    email adder

---
+++ gelweb/gel2mdt/email_handler.py
@@ -0,0 +1,8 @@
+from django.utils.log import AdminEmailHandler
+from django.core import mail
+
+
+class ManagerEmailHandler(AdminEmailHandler):
+    def send_mail(self, subject, message, *args, **kwargs):
+        mail.mail_managers(subject, '', *args, connection=self.connection(), **kwargs)
+
