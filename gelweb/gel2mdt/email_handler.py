4f68f5bfa941166564b8b6441bfffccd541fabed
Patrick Lombard <ptk.lmb55@gmail.com>
2019-04-02 10:41:44 +0100

    updated email

--- gelweb/gel2mdt/email_handler.py
+++ gelweb/gel2mdt/email_handler.py
@@ -4,5 +4,8 @@ from django.core import mail
 
 class ManagerEmailHandler(AdminEmailHandler):
     def send_mail(self, subject, message, *args, **kwargs):
-        mail.mail_managers(subject, '', *args, connection=self.connection(), **kwargs)
+        # mail.mail_managers(subject, '', *args, connection=self.connection(), **kwargs)
+        # print(subject, message)
+        msg = mail.EmailMessage(subject, '', 'gel2mdt.technicalsupport@nhs.net', ['bioinformatics@gosh.nhs.uk'])
+        msg.send()
 
